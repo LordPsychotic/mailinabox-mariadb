949092af81a53f9b6d17a0ceee8ffdb17386ce9e
LordPsychotic <ThePsychoticLord@gmail.com>
2026-04-19 18:47:56 -0500

    yet another bind9 config update

--- setup/system.sh
+++ setup/system.sh
@@ -358,8 +358,9 @@ if ! grep -q "max-recursion-queries " /etc/bind/named.conf.options; then
 	# Add a max-recursion-queries directive if it doesn't exist inside the options block.
 	sed -i "s/^}/\n\tmax-recursion-queries 100;\n}/" /etc/bind/named.conf.options
 fi
-sed -i "/listen-on-v6 { ::1; };/a \\\n\tforwarders {\n\t\t8.8.8.8;\n\t\t1.1.1.1;\n\t};" /etc/bind/named.conf.options
-
+if ! grep -q "^\s*forwarders" /etc/bind/named.conf.options; then
+	sed -i "/listen-on-v6 { ::1; };/a \\\n\tforwarders {\n\t\t8.8.8.8;\n\t\t1.1.1.1;\n\t};" /etc/bind/named.conf.options
+fi
 # First we'll disable systemd-resolved's management of resolv.conf and its stub server.
 # Breaking the symlink to /run/systemd/resolve/stub-resolv.conf means
 # systemd-resolved will read it for DNS servers to use. Put in 127.0.0.1,
